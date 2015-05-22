fd5fae2c1653d05dfc7e3304a0af9d0226636217
Benoît Baliguet <benoit.baliguet@gmail.com>
2015-05-22 16:58:57 +0200

    Rebuilding the Gwoek engine

--- play.js
+++ play.js
@@ -1,42 +1,29 @@
+/*
+ *
+ *
+ *
+ * HELPERS
+ *
+ *
+ *
+ */
+
 var $ = document.querySelector.bind(document);
 
-var ground = [];
-var nbTiles = 100;
-var tileWidth = 20;
+function setVisible(el, visible) {
+	el.style.display = visible ? "block" : "none";
+}
 
-var clouds = [];
-var cloudsCoolDown = 0;
+/*
+ *
+ *
+ *
+ * DOM ELEMENTS
+ *
+ *
+ *
+ */
 
-// game variables
-var gameOver = true;
-var withSplash = true;
-var shift = 0;
-var noVary = -1;
-var newLevel = 200;
-var darkColor = false;
-var verticalScale = 1;
-
-var playerDblJump = false;
-var playerAcceleration = 0;
-var playerHorizontalAcceleration = 0;
-var playerBottom = 100;
-var playerLeft = 120;
-var playerOnFloor = true;
-
-var now;
-var delta;
-var viewport;
-
-var score = 0;
-var topScore = 0;
-
-// limits for random ground generation
-var noVaryBase = 10;
-var nextGapTileBase = 30;
-var topLimit = 300;
-var bottomLimit = 20;
-
-// some dom elements
 var _environmentEl = $("#environment");
 var _dinoEl = $("#dino");
 var _scoreEl = $("#score");
@@ -45,93 +32,170 @@ var _twitterEl = $("#twitter");
 var _gameOverEl = $("#gameover");
 var _splashEl = $("#splash");
 
-// dom helpers
-function setVisible(el, visible) {
-	el.style.display = visible ? "block" : "none";
-}
+/*
+ *
+ *
+ *
+ * VARIABLES
+ *
+ *
+ *
+ */
+
+// game variables
+var gameOver = true;
+var withSplash = true;
+var topScore = 0;
+var darkColor = false;
+var stage;
+
+// view related
+var viewport;
+
+// environment: tiles and clouds
+var tileWidth = 20;
+
+// limits for random ground generation
+var noVaryBase = 10;
+var nextGapTileBase = 30;
+var topLimit = 300;
+var bottomLimit = 20;
 
 // random generator
 var seed = 0;
-var rand = null;
 
-function loop() {
-	var swap = false;
+/*
+ *
+ *
+ *
+ * CLASSES
+ *
+ *
+ *
+ */
 
-	updateClock();
-
-	if (shift > tileWidth * 10) {
-		// cheating !
-		gameOver = true;
+// Clock generator
+var Clock = function () {
+	this.now = Date.now();
+	this.start = this.now;
+};
+Clock.prototype.tick = function (min) {
+	var now = Date.now();
+	var dif = now - this.now;
+	if (dif > min) {
+		this.now = now;
+		return dif;
 	}
+	return 0;
+};
+Clock.prototype.total = function () {
+	return this.now - this.start;
+};
 
-	if (shift >= tileWidth) {
-		shift = Math.round(shift) % tileWidth;
-		swap = true;
-	}
+/*
+ *
+ *
+ *
+ * RENDERERS
+ *
+ *
+ *
+ */
 
-	// adjust ground
+function renderGround(ground) {
+
+}
+
+function renderClouds(clouds) {
+
+}
+
+function renderPlayer(player) {
+	/*
+	var scale = playerOnFloor ? 1 : 1 + 1 / (Math.abs(playerAcceleration / 20) + 1);
+	_dinoEl.style.bottom = playerBottom + "px";
+	_dinoEl.style.left = playerLeft + "px";
+	_dinoEl.style.transform = "scale(" + scale + ")";
+	_dinoEl.style.backgroundPosition = "-" + (14 * (Math.floor(score / 9) % 4)) + "px 0px";
+	*/
+}
+
+/*
+ *
+ *
+ *
+ * GAME RULES
+ *
+ *
+ *
+ */
+
+function updateGround(ground, dif, game) {
+	var nbTiles = ground.length;
+
+	// adjust ground left
 	ground.forEach(function (item, index) {
-		var tile = item.tile;
-		tile.style.left = index * tileWidth - shift;
-		if (swap) {
-			if (index < nbTiles - 1) {
-				item.height = ground[index + 1].height;
-			} else {
-				var withVariation = noVary < 0,
-					variation = withVariation ? 200 : 4,
-					diff = -variation / 2 + rand() * variation;
-
-				// make sure gap is a real gap
-				if (withVariation) {
-					if (diff > 0) {
-						diff += 20;
-					} else {
-						diff -= 20;
-					}
-
-					// no variation for 10 cycles
-					noVary = noVaryBase + Math.floor(rand() * nextGapTileBase);
-				}
-
-				item.height = Math.max(Math.min(item.height + diff, topLimit), bottomLimit);
-			}
-
-			var adjust = 1;
-			if (index > nbTiles * 4 / 5) {
-				adjust = (nbTiles - index) / (nbTiles / 5);
-			}
-
-			tile.style.height = adjust * item.height + "px";
-			tile.style.opacity = 0.5 + adjust / 2;
-		}
+		item.left -= dif;
 	});
 
-	shift = shift + (10 * delta);
-	noVary = noVary - delta;
+	// filter out of view tiles
+	ground = ground.filter(function (item) {
+		return item.left > -tileWidth;
+	});
 
+	// insert new tiles
+	while (ground.length < nbTiles) {
+		var lastTile = ground[ground.length - 1];
+		var newTile = {
+			left: lastTile.left + tileWidth
+		};
+
+		var withVariation = game.noVary < 0;
+		var variation = withVariation ? 200 : 4;
+		var diff = -variation / 2 + game.rand() * variation;
+
+		// make sure gap is a real gap
+		if (withVariation) {
+			if (diff > 0) {
+				diff += 20;
+			} else {
+				diff -= 20;
+			}
+
+			// no variation for 10 cycles
+			game.noVary = noVaryBase + Math.floor(game.rand() * nextGapTileBase);
+		} else {
+			// update the noVary value
+			game.noVary--;
+		}
+
+		newTile.height = Math.max(Math.min(lastTile.height + diff, topLimit), bottomLimit);
+		ground.push(newTile);
+	}
+
+	return ground;
+}
+
+function updateClouds(clouds, dif, game) {
 	// adjust clouds
-	if (!cloudsCoolDown) {
+	if (!game.cloudsCoolDown) {
 		// can create new cloud
-		cloudsCoolDown = false;
+		game.cloudsCoolDown = false;
 		if (Math.random() < 0.5) {
-			cloudsCoolDown = 150;
-			var cloud = document.createElement("div");
-			cloud.classList.add("cloud");
-			cloud.style.bottom = Math.floor((viewport.height - topLimit - 50) * Math.random()) + topLimit + 25;
-			_environmentEl.appendChild(cloud);
-			clouds.push({
-				cloud: cloud,
-				left: viewport.width
-			});
+			game.cloudsCoolDown = 150;
+			var cloud = {
+				bottom: Math.floor((viewport.height - topLimit - 50) * Math.random()) + topLimit + 25,
+				left: viewport.left
+			};
+			clouds.push(cloud);
 		}
 	} else {
-		cloudsCoolDown--;
+		game.cloudsCoolDown--;
 	}
 
 	// adjuste cloud position
 	clouds.forEach(function (cloud) {
-		cloud.left -= delta * 2;
-		cloud.cloud.style.left = cloud.left;
+		cloud.left -= dif * 2;
 	});
 
 	// remove dead clouds
@@ -139,138 +203,148 @@ function loop() {
 		return cloud.left > -200;
 	});
 
-	// adjust player on its tile
-	var playerTile = ground[Math.floor((playerLeft + 14) / tileWidth)],
-		target = playerTile ? playerTile.height : 0,
-		diff = playerBottom - target,
-		absoluteDiff = diff < 0 ? -diff : diff;
+	return clouds;
+}
 
-	if (!gameOver && playerTile && absoluteDiff < 20 && playerOnFloor) {
-		playerBottom = target;
-		playerAcceleration = 0;
-		playerOnFloor = true;
-		playerDblJump = false;
-	} else if (!gameOver && playerTile && diff > 0) {
-		playerAcceleration = playerAcceleration + delta;
-		playerBottom = playerBottom - (delta * playerAcceleration);
-		if (playerBottom - target < 3) {
-			playerBottom = target;
-			playerOnFloor = true;
-			playerDblJump = false;
+function updatePlayer(player, dif, game, ground) {
+
+	// adjust player on its tile
+	var playerTile = ground[Math.floor((player.left + 14) / tileWidth)];
+	var target = playerTile ? playerTile.height : 0;
+	var diff = player.bottom - target;
+	var absoluteDiff = diff < 0 ? -diff : diff;
+
+	if (playerTile && absoluteDiff < 20 && player.onFloor) {
+		player.bottom = target;
+		player.acceleration = 0;
+		player.onFloor = true;
+		player.dblJump = false;
+	} else if (playerTile && diff > 0) {
+		player.acceleration = player.acceleration + dif;
+		player.bottom = player.bottom - player.acceleration;
+		if (player.bottom - target < 3) {
+			player.bottom = target;
+			player.onFloor = true;
+			player.dblJump = false;
 		}
 	} else {
-		playerHorizontalAcceleration = -8;
-		if (!playerTile || gameOver) {
-			// GAME OVER
+		player.horizontalAcceleration = -8;
+		if (!playerTile) {
 			gameOver = true;
-			score = Math.floor(score);
-			ga("send", "event", "score", "session", "Score", score);
-			if (score > topScore) {
-				topScore = score;
-				_topScoreEl.innerHTML = "Top score: " + topScore;
-				ga("send", "event", "score", "top", "Top score", topScore);
-			}
-
-			_dinoEl.style.transform = "scale(1)";
-			setVisible(_gameOverEl, true);
-			setVisible(_dinoEl, false);
-			document.body.classList.add("gameover");
-			_twitterEl.href = "https://twitter.com/home?status=" +
-				encodeURIComponent("Just scored " + score + " on Gwoek! Challenge me on this track here: http://bbaliguet.github.io/Gwoek/#" + seed);
-
-			// 2s before restart with space
-			setTimeout(function () {
-				withSplash = true;
-			}, 2000);
 			return;
 		} else {
-			playerBottom = playerTile.height;
+			player.bottom = playerTile.height;
 		}
 	}
 
 	// adjust player left
-	playerHorizontalAcceleration += delta / 3;
-	if (playerHorizontalAcceleration > 1) {
-		playerHorizontalAcceleration = 1;
+	player.horizontalAcceleration += dif;
+	if (player.horizontalAcceleration > 1) {
+		player.horizontalAcceleration = 1;
+	}
+	player.left += player.horizontalAcceleration / 10;
+	if (player.left > 120) {
+		player.left = 120;
 	}
 
-	playerLeft += playerHorizontalAcceleration * delta;
-	if (playerLeft > 120) {
-		playerLeft = 120;
-	}
-
-	var scale = playerOnFloor ? 1 : 1 + 1 / (Math.abs(playerAcceleration / 20) + 1);
-	_dinoEl.style.bottom = playerBottom + "px";
-	_dinoEl.style.left = playerLeft + "px";
-	_dinoEl.style.transform = "scale(" + scale + ")";
-
-	// adjust score
-	score = score + delta;
-	if (newLevel < 0) {
-		newLevel = 200;
-		noVaryBase = noVaryBase * 0.8;
-		nextGapTileBase = nextGapTileBase * 0.8;
-	} else {
-		newLevel = newLevel - delta;
-	}
-
-	// adjust sprite (4 phases, 14px width)
-	_dinoEl.style.backgroundPosition = "-" + (14 * (Math.floor(score / 9) % 4)) + "px 0px";
-	_scoreEl.innerHTML = "score: " + Math.floor(score);
-
-	requestAnimationFrame(loop);
+	return player;
 }
 
-function updateClock() {
-	var previous = now;
-	if (now) {
-		now = Date.now();
-		delta = (now - previous) * 60 / 1000;
-	} else {
-		now = Date.now();
-		delta = 1;
-	}
+function updateEnv(stage, dif, game) {
+	// adjust ground
+	stage.ground = updateGround(stage.ground, dif, stage.game);
+	// adjust clouds
+	stage.clouds = updateClouds(stage.clouds, dif, stage.game);
+	// adjust player
+	stage.player = updatePlayer(stage.player, dif, stage.game, stage.ground);
 }
+
+/*
+ *
+ *
+ *
+ * GAME LOOP
+ *
+ *
+ *
+ */
+
+function loop() {
+
+	var clock = stage.clock;
+	var dif;
+
+	// update the environment
+	do {
+		dif = stage.clock.tick(stage.game.speed);
+		if (dif) {
+			updateEnv(stage, dif);
+		}
+	} while(dif);
+
+	// update the display
+	renderGround(stage.ground);
+	renderClouds(stage.clouds);
+	renderPlayer(stage.player);
+
+	_scoreEl.innerHTML = "score: " + Math.floor(clock.total());
+
+	if (gameOver) {
+		return;
+	}
+
+	requestAnimationFrame(function () {
+		loop();
+	});
+}
+
+/*
+ *
+ *
+ *
+ * GAME STATES
+ *
+ *
+ *
+ */
 
 function init() {
 	document.body.classList.remove("gameover");
 	setVisible(_gameOverEl, false);
 	setVisible(_splashEl, false);
 	setVisible(_dinoEl, true);
+
+	// init viewport
 	viewport = document.body.getBoundingClientRect();
 	gameOver = false;
 
 	// init ground
-	nbTiles = Math.floor(viewport.width / 20) + 2;
-	_environmentEl.innerHTML = "";
-	ground.splice(0, ground.length);
+	var ground = [];
+	var nbTiles = Math.floor(viewport.width / tileWidth) + 1;
 	for (var i = 0; i < nbTiles; i++) {
-		var tile = document.createElement("div");
 		ground.push({
 			height: 100,
-			tile: tile
+			left: i * tileWidth
 		});
-		tile.classList.add("ground");
-		_environmentEl.appendChild(tile);
 	}
 
-	// init clouds
-	clouds.splice(0, clouds.length);
+	stage = {
+		ground: ground,
+		clouds: [],
+		player: {
+			dblJump: false,
+			acceleration: 0,
+			horizontalAcceleration: 0,
+			bottom: 100,
+			onFloor: true,
+			left: 120
+		},
+		game: {
+			speed: 10
+		},
+		clock: new Clock()
+	};
 
-	// init player
-	playerDblJump = false;
-	playerAcceleration = 0;
-	playerHorizontalAcceleration = 0;
-	playerBottom = 100;
-	playerOnFloor = true;
-	score = 0;
-	playerLeft = 120;
-	noVaryBase = 10;
-	nextGapTileBase = 30;
-
-	// init clock
-	now = 0;
-	delta = 1;
 
 	// init rand method. Use seed if provided
 	var hash = parseInt(window.location.hash.substr(1), 10);
@@ -280,14 +354,48 @@ function init() {
 	}
 
 	var generator = new MersenneTwister(seed);
-	rand = function () {
+	stage.game.rand = function () {
 		return generator.random();
 	};
 	window.location.hash = "#" + seed;
+	
+	
 
 	// start loop
 	loop();
 }
+
+function showGameOver(game) {
+	var score = Math.floor(stage.clock.total());
+	ga("send", "event", "score", "session", "Score", score);
+	if (score > topScore) {
+		topScore = score;
+		_topScoreEl.innerHTML = "Top score: " + topScore;
+		ga("send", "event", "score", "top", "Top score", topScore);
+	}
+
+	_dinoEl.style.transform = "scale(1)";
+	setVisible(_gameOverEl, true);
+	setVisible(_dinoEl, false);
+	document.body.classList.add("gameover");
+	_twitterEl.href = "https://twitter.com/home?status=" +
+		encodeURIComponent("Just scored " + score + " on Gwoek! Challenge me on this track here: http://bbaliguet.github.io/Gwoek/#" + seed);
+
+	// 2s before restart with space
+	setTimeout(function () {
+		withSplash = true;
+	}, 2000);
+}
+
+/*
+ *
+ *
+ *
+ * ACTION HANDLERS
+ *
+ *
+ *
+ */
 
 function onAction() {
 	if (gameOver) {
@@ -299,18 +407,20 @@ function onAction() {
 		return;
 	}
 
-	if (!playerOnFloor && playerDblJump) {
-		if (playerAcceleration > 0) {
-			playerAcceleration = 0;
+	var player = stage.player;
+	
+	if (!player.onFloor && player.dblJump) {
+		if (player.acceleration > 0) {
+			player.acceleration = 0;
 		}
 	} else {
-		if (playerOnFloor) {
-			playerOnFloor = false;
-			playerAcceleration = -20;
-			playerBottom += 10;
+		if (player.onFloor) {
+			player.onFloor = false;
+			player.acceleration = -20;
+			player.bottom += 10;
 		} else {
-			playerDblJump = true;
-			playerAcceleration = -15;
+			player.dblJump = true;
+			player.acceleration = -15;
 			if (darkColor || Math.random() < 0.5) {
 				darkColor = !darkColor;
 				var classList = document.body.classList;
